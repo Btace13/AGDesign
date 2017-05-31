781975a77ce7a8babfa5150ccc1aeb8e931bdc0d
Enrique Urbina <btace1347@yahoo.com>
2017-05-31 15:11:18 -0500

    fixing nav problem

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Landed by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function($) {
 
@@ -49,10 +44,10 @@
 			});
 
 		// Dropdowns.
-			$('#nav > ul').dropotron({
-				alignment: 'right',
-				hideDelay: 350
-			});
+			// $('#nav > ul').dropotron({
+			// 	alignment: 'right',
+			// 	hideDelay: 350
+			// });
 
 		// Off-Canvas Navigation.
 
